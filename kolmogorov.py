0ac4ab2327ffacc68f303de5ca4e8dd5644a2e5d
Spruce Bondera <sprucebondera@gmail.com>
2017-01-15 21:25:43 -0500

    Initial commit, finding smallest programs

---
+++ kolmogorov.py
@@ -0,0 +1,42 @@
+import math
+import sys
+import subprocess
+
+def int_to_bytes(integer):
+    # size in bytes
+    size = 1 if integer == 0 else math.ceil(math.log2(integer + 1) / 8)
+    return integer.to_bytes(size, sys.byteorder)
+
+def possible_bytes():
+    '''Generates all possible strings in numerical byte order'''
+    program_integer = 0
+    while True:
+        program_bytes = int_to_bytes(program_integer)
+        yield program_bytes
+        program_integer += 1
+
+def possible_programs(filename):
+    '''Writes all possible programs to a file, writing the next one on next()'''
+    possibilities = possible_bytes()
+    for program in possibilities:
+        with open(filename, 'wb') as program_file:
+            program_file.write(program)
+        yield program
+
+def attempt_compile(filename, executable):
+    return subprocess.run(['g++', filename, '-o', executable],
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode == 0
+
+def main():
+    program_name = 'program.cpp'
+    last_length = 0
+    for program in possible_programs(program_name):
+        if len(program) != last_length:
+            print(len(program))
+            last_length = len(program)
+        if attempt_compile(program_name, 'program'):
+            print('Finished!')
+            return
+
+if __name__ == '__main__':
+    main()
